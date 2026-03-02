0827c0e6411e32a689c8abf30c719cdd132280f0
Kamalveer singh <kamalhara7@gmail.com>
2026-03-02 17:25:32 +0530

    initial commit

--- README.md
+++ README.md
@@ -85,4 +85,3 @@ productify/
 ## License
 
 MIT
-# productify
---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    result: "success",
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3000");
+});
